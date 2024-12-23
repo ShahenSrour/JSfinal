c62934a4ddc5c53579cced48e4ed520ed50dcc8e
ShahenSrour <shahenmohdturki@gmail.com>
2024-12-23 18:46:41 +0400

    added search button

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ fetch('https://emojihub.yurace.pro/api/all').then(res => { return res.json() }).
         .map((name) => {
             return `
         <section class="card">
-        <h1>${name.htmlCode}</h1>
+        <h1 class="emoji">${name.htmlCode}</h1>
             <h1>emoji name :  ${name.name}</h1>
             <h1>emoji category : ${name.category}</h1>
         </section>
@@ -17,22 +17,26 @@ fetch('https://emojihub.yurace.pro/api/all').then(res => { return res.json() }).
 
 function OnSearchChange(event) {
     let cat = event.target.value
-    if (cat != "smileys-and-people"|| cat != "animals-and-nature"|| cat != "food-and-drink"|| cat != "travel-and-place"|| cat != "activities"|| cat != "objects"|| cat != "symbols"|| cat != "flags") {
+    if ((cat == "smileys-and-people") || (cat == "animals-and-nature") || (cat == "food-and-drink") || (cat == "travel-and-place") || (cat == "activities") || (cat == "objects") || (cat == "symbols") || (cat == "flags")) {
+        fetch(`https://emojihub.yurace.pro/api/all/category/${cat}`).then(res => { return res.json() }).then(data => {
+            console.log(data)
+            emojilist.innerHTML = data
+                .map((name) => {
+                    return `
+            <section class="card">
+                <h1>${name.htmlCode}</h1>
+                <h1>emoji name :  ${name.name}</h1>
+                <h1>emoji category : ${name.category}</h1>
+            </section>
+            `
+                })
+                .join('');
+        })
+    }
+    else {
         console.log("error, no valid search results")
         alert("error, no valid search results")
+
     }
-    fetch(`https://emojihub.yurace.pro/api/all/category/${cat}`).then(res => { return res.json() }).then(data => {
-        console.log(data)
-        emojilist.innerHTML = data
-            .map((name) => {
-                return `
-        <section class="card">
-            <h1>${name.htmlCode}</h1>
-            <h1>emoji name :  ${name.name}</h1>
-            <h1>emoji category : ${name.category}</h1>
-        </section>
-        `
-            })
-            .join('');
-    })
+
 }
