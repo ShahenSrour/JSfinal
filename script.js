8ccab7b4fa4454e7b29c476ecc36d114e28fd962
ShahenSrour <shahenmohdturki@gmail.com>
2024-12-23 14:44:17 +0400

    submission 2

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ fetch('https://emojihub.yurace.pro/api/all').then(res => { return res.json() }).
     })
     .join('');
 })
+
+function OnSearchChange(event) {
+   let cat = event.target.value
+   fetch(`https://emojihub.yurace.pro/api/all/category/${cat}`).then(res => { return res.json() }).then(data => {
+    console.log(data)
+    emojilist.innerHTML = data
+    .map((name) => {
+        return `
+        <section class="card">
+            <h1>emoji name :  ${name.name}</h1>
+            <h1>emoji category : ${name.category}</h1>
+        </section>
+        `
+    })
+    .join('');
+})
+}
